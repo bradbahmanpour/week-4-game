c025441704dd1b141c8d20b578b6f8b4c591a042
Brad Bahmanpour <bradbahmanpour@Brads-MacBook-Pro.local>
2018-02-19 23:01:15 -0800

    modifed CSS file

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,106 +1,122 @@
 
-// Create an array of words for HANGMAN
 
-var words = ['italy', 'usa', 'france', 'norway', 'germany', 'denmark', 'finland'];
+$(document).ready(function () {
 
-// Variables for tracking our wins, losses and ties. They begin at 0.
-var wins = 0;
-var guesses = 10;
-var numberofGuess = 0;
-var computerGuess = "";
-var dashes = "-";
-var currentWord = "-";
-var computerChoice = "";
-var userGuessString = "";
+  var computerRandomNumber = 0;
+  var crystalRandomNumber = 0;
+  var userScore = 0;
+  var crystalOneRandomNumber = 0;
+  var crystalTwoRandomNumber = 0;
+  var crystalThreeRandomNumber = 0;
+  var crystalFourRandomNumber = 0;
+  var userWin = 0;
+  var userLose = 0;
 
-function compareLetters(userInput, computerString) {
 
-    var index = 0;
 
-    while (index < computerString.length) {
-        if (userInput !== computerString.charAt(index)) {
-            console.log(computerString.charAt(index));
-            index++;
-        } else {
-            return index;
-        }
-    }
-    return -1;
-};
+  function blinker() {
+    $('.blink_me').fadeOut(500);
+    $('.blink_me').fadeIn(500);
+  }
+  setInterval(blinker, 1000);
 
-function replaceAt(string, index, replace) {
-    return string.substring(0, index) + replace + string.substring(index + 1);
-};
 
-function selectCountry() {
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    computerGuess = "";
-    computerChoice = words[Math.floor(Math.random() * words.length)];
-    console.log("Computer Choice is " + computerChoice);
-    console.log("The Length of the Computer Choice String is " + computerChoice.length);
+  // this function generate random number between min - max numbers
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-    // Add Dashes to Display in CurrentWord field
-    for (i = 0; i < computerChoice.length; i++) {
-        computerGuess = replaceAt(computerGuess, i, "-");
-    }
+  // this function generate random number between 1 to 12 for Crystal One 
+  function createCrystalsRandomNumbers() {
+    crystalOneRandomNumber = randomIntFromInterval(1, 12);
+    console.log(crystalOneRandomNumber);
+    // this function generate random number between 1 to 12 for Crystal Two 
+    crystalTwoRandomNumber = randomIntFromInterval(1, 12);
+    console.log(crystalTwoRandomNumber);
+    // this function generate random number between 1 to 12 for Crystal Three 
+    crystalThreeRandomNumber = randomIntFromInterval(1, 12);
+    console.log(crystalThreeRandomNumber);
+    // this function generate random number between 1 to 12 for Crystal Four 
+    crystalFourRandomNumber = randomIntFromInterval(1, 12);
+    console.log(crystalFourRandomNumber);
+  }
+
+  // this function to initialize the values and reset the app
+  function initializeThis() {
+    computerRandomNumber = 0;
+    crystalRandomNumber = 0;
+    userScore = 0;
+    crystalOneRandomNumber = 0;
+    crystalTwoRandomNumber = 0;
+    crystalThreeRandomNumber = 0;
+    crystalFourRandomNumber = 0;
     
-    document.getElementById('currentWord').innerHTML = computerGuess;
-};
 
-function resetVariables() {
 
-    document.getElementById('userGuess').innerHTML = null;
-    document.getElementById('currentWord').innerHTML = null;
-    // computerGuess = null;
-    selectCountry();
-    guesses = 10;
-    userGuessString = null;
+    $("#randomNumber, #totalScore").empty();
 
-}
+    // Generate the Computer random number
+    computerRandomNumber = randomIntFromInterval(19, 120);
+    // Generate the random numbers for each crystal
+    createCrystalsRandomNumbers();
 
-resetVariables();
-// When the user presses a key, it will run the following function...
-document.onkeypress = function (event) {
+    console.log("Computer Random number is = " + computerRandomNumber);
+    $("#randomNumber").text(computerRandomNumber);
 
-    // Determines which key was pressed.
-    var userGuessedLetter = event.key;
-    guesses--;
+  }
 
-    var letterLocation = compareLetters(userGuessedLetter, computerChoice);
-    console.log("letterLocation  = " + letterLocation);
+  function checkScore() {
 
-    if (letterLocation >= 0) {
+    if (userScore === computerRandomNumber) {
+      userWin++;
+      $("#wins").text(userWin);
 
-        computerGuess = replaceAt(computerGuess, letterLocation, userGuessedLetter);
-
-        console.log("userGuessedLetter =" + userGuessedLetter);
-
-        console.log("ComputerGuess =" + computerGuess);
-        document.getElementById('currentWord').innerHTML = computerGuess;
-        //  guesses = 10;
-
+      $("h2:first").replaceWith("<h2 class=\"text-center, blink_me\">You Won!</h2>").delay(3000);
+      initializeThis();
     }
+    else if (userScore > computerRandomNumber) {
+      userLose++;
+      $("#losses").text(userLose);
+      $("h2:first").replaceWith("<h2 class=\"text-center, blink_me\">You Lost!</h2>").delay(3000);
 
-    document.getElementById('numberofGuess').innerHTML = guesses;
-
-    if (userGuess != computerChoice) {
-        userGuessString = document.getElementById('userGuess').innerHTML;
-        userGuessString = userGuessString + userGuessedLetter;
-        console.log("userGuessString = " + userGuessString);
-
-        document.getElementById('userGuess').innerHTML = userGuessString + ",";
+      initializeThis();
     }
-    console.log("computerGuess = " + computerGuess);
-    console.log("computerChoice = " + computerChoice);
+  }
 
-    if (computerGuess  === computerChoice) {
-        wins++;
-        document.getElementById('userWins').innerHTML = wins;
-        resetVariables();
-    }
+  initializeThis();
 
-    if (guesses === 0) {
-        resetVariables();
-    }
+  // Add an on click listener to Crystals
+  $("#crystal1").on("click", function () {
+    $("h2:first").replaceWith("<h2></h2>");
+    userScore = userScore + crystalOneRandomNumber;
+    console.log(userScore);
+    $("#totalScore").text(userScore);
+    checkScore();
+  });
 
-}
+  $("#crystal2").on("click", function () {
+    $("h2:first").replaceWith("<h2></h2>");
+    userScore = userScore + crystalTwoRandomNumber;
+    console.log(userScore);
+    $("#totalScore").text(userScore);
+    checkScore();
+  });
+
+  $("#crystal3").on("click", function () {
+    $("h2:first").replaceWith("<h2></h2>");
+    userScore = userScore + crystalThreeRandomNumber;
+    console.log(userScore);
+    $("#totalScore").text(userScore);
+    checkScore();
+  });
+
+  $("#crystal4").on("click", function () {
+    $("h2:first").replaceWith("<h2></h2>");
+    userScore = userScore + crystalFourRandomNumber;
+    console.log(userScore);
+    $("#totalScore").text(userScore);
+    checkScore();
+  });
+
+});
+
